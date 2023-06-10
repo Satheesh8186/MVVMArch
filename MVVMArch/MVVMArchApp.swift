bb3be8842f0782641265051d06726e90c4b77786
satheesh <sknaidu8186@gmail.com>
2023-06-10 14:12:18 -0400

    Initial Commit

---
+++ MVVMArch/MVVMArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMArchApp.swift
+//  MVVMArch
+//
+//  Created by satheesh kumar on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
